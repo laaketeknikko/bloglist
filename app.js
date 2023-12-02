0ac9b02bb7b33f89ca9c0dd092cd95135a61c826
laaketeknikko <139527598+laaketeknikko@users.noreply.github.com>
2023-12-02 17:46:07 +0200

    exercise 4.3 complete

---
+++ app.js
@@ -0,0 +1,24 @@
+const config = require("./utils/config")
+const express = require("express")
+const app = express()
+const cors = require('cors')
+
+const blogsRouter = require("./controllers/blogs")
+
+const mongoose = require('mongoose')
+
+
+
+mongoose.set("strictQuery", false)
+const mongoUrl = config.MONGODB_URI
+mongoose.connect(mongoUrl)
+
+
+app.use(cors())
+app.use(express.json())
+
+app.use("", blogsRouter)
+
+
+
+module.exports = app;
