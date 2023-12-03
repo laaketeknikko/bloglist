646145952dd32c890cd0ae44526c69c46daa89d7
laaketeknikko <139527598+laaketeknikko@users.noreply.github.com>
2023-12-03 13:46:25 +0200

    exercise 4.7 complete

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -63,4 +63,61 @@ describe("Most blogs", () => {
         const result = listHelper.mostBlogs(blogTestData)
         expect(result).toBe("Robert C. Martin")
     })
+
+    test("with no blogs", () => {
+        const result = listHelper.mostBlogs([])
+        expect(result).toBe("")
+    })
+
+    test("with one blog", () => {
+        const result = listHelper.mostBlogs([blogTestData[0]])
+        expect(result).toBe("Michael Chan")
+    })
+})
+
+
+describe("Most likes", () => {
+
+    test("with no blogs", () => {
+        const result = listHelper.mostLikes([])
+        expect(result).toBe(null)
+    })
+
+    test("with no blogs, Lodash version", () => {
+        const result = listHelper.mostLikes_lodash([])
+        expect(result).toBe(null)
+    })
+
+    test("with one blog", () => {
+        const result = listHelper.mostLikes([blogTestData[0]])
+        expect(result).toEqual({
+            author: blogTestData[0].author,
+            likes: blogTestData[0].likes
+        })
+    })
+
+    test("with one blog, Lodash version", () => {
+        const result = listHelper.mostLikes_lodash([blogTestData[1]])
+        expect(result).toEqual({
+            author: blogTestData[1].author,
+            likes: blogTestData[1].likes
+        })
+    })
+
+    test("with many blogs", () => {
+        const result = listHelper.mostLikes(blogTestData)
+        expect(result).toEqual({
+            author: "Edsger W. Dijkstra",
+            likes: 17
+        })
+    })
+
+    test("with many blogs", () => {
+        const result = listHelper.mostLikes_lodash(blogTestData)
+        expect(result).toEqual({
+            author: "Edsger W. Dijkstra",
+            likes: 17
+        })
+    })
+
 })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,7 @@
+const lodash = require('lodash');
+
+
+
 const dummy = (blogs) => {
     return 1
 }
@@ -28,7 +32,7 @@ const favouriteBlog = (blogs) => {
 // Probably not for large arrays
 const mostBlogs = (blogs) => {
     if (!blogs || blogs.length === 0) {
-        return  []
+        return  ""
     }
     // First reduce blogs to a map of author:blogs
     let result = blogs.reduce((totals, current) => {
@@ -53,9 +57,70 @@ const mostBlogs = (blogs) => {
 }
 
 
+const mostLikes = (blogs) => {
+    if (!blogs || blogs.length === 0) {
+        return null
+    }
+    // First reduce blogs to a map of author:blogs
+    let result = blogs.reduce((totals, current) => {
+        totals[current.author]?
+        totals[current.author] += current.likes : 
+        totals[current.author] = current.likes
+        
+        return totals
+    }, {})
+
+    // Then reduce by number of blogs
+    result = Object.entries(result)
+   .reduce((max, current) => {
+    if (current[1] >= max[1]) {
+        max[0] = current[0]
+        max[1] = current[1]
+    }
+    return max
+   })
+
+   return {
+    author: result[0],
+    likes: result[1]
+   }
+}
+
+
+const mostLikes_lodash = (blogs) => {
+
+    if (!blogs || blogs.length === 0) {
+        return null
+    }
+
+    // Returns an array with authors as keys
+    const result = lodash.groupBy(blogs, (blog) => {
+        return blog.author
+    })
+
+    const maxLikes = Object.entries(result).reduce((mostLiked, current) => {
+        const authorLikes = current[1].reduce((totalLikes, current) => {
+            return current.likes + totalLikes
+        }, 0)
+
+        if (authorLikes > mostLiked.likes) {
+            mostLiked.author = current[0]
+            mostLiked.likes = authorLikes
+        }
+        return mostLiked
+        
+    }, {author: "", likes: 0})
+    
+    
+    console.log(maxLikes)
+    return maxLikes
+}
+
 module.exports = {
     dummy,
     totalLikes,
     favouriteBlog,
-    mostBlogs
+    mostBlogs,
+    mostLikes,
+    mostLikes_lodash
 }
