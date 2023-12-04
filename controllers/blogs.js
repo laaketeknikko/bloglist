c7ee3baa1d6198d8be6bb24aa9de8efc3bb752e7
laaketeknikko <139527598+laaketeknikko@users.noreply.github.com>
2023-12-04 07:31:23 +0200

    exercise 4.10 complete

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,16 +2,6 @@ const blogsRouter = require("express").Router();
 const Blog = require("../mongoose_models/Blog");
 
 
-/*
-blogsRouter.get('/api/blogs', (request, response) => {
-    Blog
-      .find({})
-      .then(blogs => {
-        response.json(blogs)
-      })
-  })
-  
-  */
 
 blogsRouter.get('/api/blogs', async (request, response) => {
     const blogs = await Blog.find({})
@@ -19,14 +9,11 @@ blogsRouter.get('/api/blogs', async (request, response) => {
 })
 
 
-blogsRouter.post('/api/blogs', (request, response) => {
+blogsRouter.post('/api/blogs', async (request, response) => {
     const blog = new Blog(request.body)
 
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
+    const result = await blog.save()
+    response.status(201).json(result)
 })
 
 
