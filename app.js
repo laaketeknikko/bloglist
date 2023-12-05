7fb5837fee576306c9dee186ce93db83d4104b4c
laaketeknikko <139527598+laaketeknikko@users.noreply.github.com>
2023-12-05 17:52:43 +0200

    exercise 4.18 complete

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 
 const blogsRouter = require("./controllers/blogs")
 const usersRouter = require("./controllers/users")
+const loginRouter = require("./controllers/login")
 
 const mongoose = require('mongoose')
 mongoose.set("strictQuery", false)
@@ -15,6 +16,7 @@ mongoose.connect(mongoUrl)
 app.use(cors())
 app.use(express.json())
 
+app.use("/api/login", loginRouter)
 app.use("/api/blogs", blogsRouter)
 app.use("/api/users", usersRouter)
 
